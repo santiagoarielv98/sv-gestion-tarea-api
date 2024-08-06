e2e43ffe0c4ae969e5614384c01ecf7cb7c7f925
Santiago Ariel <santiagoarielv98@gmail.com>
2024-08-06 15:14:53 -0300

    chore: Add CORS middleware for allowing requests from specified origin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express, { json, urlencoded } from "express";
 import logger from "morgan";
+import cors from "cors";
 
 import router from "./routes/index.js";
 const app = express();
@@ -9,6 +10,7 @@ app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
 
 app.use("/", router);
 
