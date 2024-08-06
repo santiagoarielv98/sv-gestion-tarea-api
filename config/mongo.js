0dc863c3e0bb35820ae3e83e273c3ac548550c4b
Santiago Ariel <santiagoarielv98@gmail.com>
2024-08-06 11:19:48 -0300

    chore: Update MongoDB connection options in config/mongo.js

--- config/mongo.js
+++ config/mongo.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
+const uri = process.env.MONGODB_URI;
+
 const connectDB = async () => {
+  const clientOptions = {
+    serverApi: { version: "1", strict: true, deprecationErrors: true },
+  };
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
     console.log("MongoDB connected");
   } catch (error) {
+    await mongoose.disconnect();
     console.error("MongoDB connection failed");
     process.exit(1);
   }
