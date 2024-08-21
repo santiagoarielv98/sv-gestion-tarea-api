b6c62b3144bb6dbc2f9ebffe97ec67daa1454f08
Santiago Ariel <santiagoarielv98@gmail.com>
2024-08-21 19:12:11 -0300

    chore: Update COOKIE_OPTIONS in constants.js to include "sameSite: none" for secure cookies

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,11 @@
 export const PRIORITIES = ["low", "medium", "high", "urgent"];
 export const AUTH_COOKIE_NAME = "access_token";
 
+/**
+ * @type {import('express').CookieOptions}
+ */
 export const COOKIE_OPTIONS = {
   httpOnly: true,
+  sameSite: "none",
+  secure: process.env.NODE_ENV === "production",
 };
