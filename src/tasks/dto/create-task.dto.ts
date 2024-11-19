3abb9cec6b5a80bc30b3c937675cccc86e2a49d0
Santiago Ariel <santiagoarielv98@gmail.com>
2024-11-18 23:44:13 -0300

    feat: Add class-validator and class-transformer for DTO validation in tasks module

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTaskDto {}
+import { IsNotEmpty, IsString, IsOptional } from "class-validator";
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  content?: string;
+}
