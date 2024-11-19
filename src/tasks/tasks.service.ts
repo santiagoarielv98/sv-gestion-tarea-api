5639fe86105b6f476fe7ec73c2c961274cd5e493
Santiago Ariel <santiagoarielv98@gmail.com>
2024-11-18 23:56:23 -0300

    feat: Integrate Prisma service for task management and update tasks module

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { UpdateTaskDto } from "./dto/update-task.dto";
+import { PrismaService } from "src/prisma.service";
 
 @Injectable()
 export class TasksService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+    return this.prismaService.task.create({
+      data: createTaskDto,
+    });
   }
 
   findAll() {
-    return `This action returns all tasks`;
+    return this.prismaService.task.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} task`;
+    return this.prismaService.task.findUnique({
+      where: { id },
+    });
   }
 
   update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+    return this.prismaService.task.update({
+      where: { id },
+      data: updateTaskDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} task`;
+    return this.prismaService.task.delete({
+      where: { id },
+    });
   }
 }
