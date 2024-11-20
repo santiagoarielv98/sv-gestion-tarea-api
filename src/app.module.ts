a35f1e307452ddaff91d8ca2c628377542941987
Santiago Ariel <santiagoarielv98@gmail.com>
2024-11-20 14:04:40 -0300

    feat: Add User model and implement Users module with basic CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { TasksModule } from "./tasks/tasks.module";
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, UsersModule],
   controllers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*",
+  });
+
   app.setGlobalPrefix("api");
   app.useGlobalPipes(
     new ValidationPipe({
