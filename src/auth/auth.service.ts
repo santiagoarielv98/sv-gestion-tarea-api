e26efdd296f2d4a46fd2bd9e772e017ad43acb30
Santiago Ariel <santiagoarielv98@gmail.com>
2024-11-21 11:28:42 -0300

    feat: Refactor user registration to use ConfigService for SALT_ROUNDS and handle email conflicts during user creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,12 +4,14 @@ import * as bcrypt from "bcrypt";
 import { JwtService } from "@nestjs/jwt";
 import { User } from "src/users/interfaces/user.interface";
 import { CreateUserDto } from "src/users/dto/create-user.dto";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private configService: ConfigService
   ) {}
 
   async validateUser(email: string, pass: string): Promise<User> {
@@ -34,13 +36,15 @@ export class AuthService {
   async register(user: CreateUserDto) {
     const hashedPassword = await bcrypt.hash(
       user.password,
-      process.env.SALT_ROUNDS
+      parseInt(this.configService.get("SALT_ROUNDS"))
     );
+
     const newUser = await this.usersService.create({
       ...user,
       password: hashedPassword,
     });
 
-    return this.login(newUser);
+    // return this.login(newUser);
+    return newUser;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,12 @@
-import { Injectable } from "@nestjs/common";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma.service";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  findOne(email: string) {
+  async findOne(email: string) {
     return this.prismaService.user.findUnique({
       where: {
         email: email,
@@ -15,7 +14,13 @@ export class UsersService {
     });
   }
 
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.findOne(createUserDto.email);
+
+    if (user) {
+      throw new ConflictException("El correo electrónico ya está en uso.");
+    }
+
     return this.prismaService.user.create({
       data: createUserDto,
     });
