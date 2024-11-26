f38e2a8708f3c8b3267a3389dd841dd7f86bf241
Santiago Ariel <santiagoarielv98@gmail.com>
2024-11-26 09:27:30 -0300

    feat: Add class-transformer Type decorator to PaginationDto for type safety

--- src/pagination/dto/pagination.dto.ts
+++ src/pagination/dto/pagination.dto.ts
@@ -1,13 +1,16 @@
+import { Type } from "class-transformer";
 import { IsInt, IsOptional, Min, IsString } from "class-validator";
 
 export class PaginationDto {
   @IsOptional()
   @IsInt()
+  @Type(() => Number)
   @Min(1)
   page?: number = 1;
 
   @IsOptional()
   @IsInt()
+  @Type(() => Number)
   @Min(1)
   limit?: number = 10;
 }
